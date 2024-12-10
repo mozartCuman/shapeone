916781127c9a40e0a2493d0c958294ce9f153036
Felix Augusto <111309593+AugustoFO@users.noreply.github.com>
2024-12-10 12:10:19 -0300

    Adicionando atributos e criando relacionamento com a tabela Treinos

--- src/main/java/com/shapeone/shapeone/model/Usuario.java
+++ src/main/java/com/shapeone/shapeone/model/Usuario.java
@@ -1,9 +1,16 @@
 package com.shapeone.shapeone.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -22,25 +29,33 @@ public class Usuario {
 	@NotNull(message = "O Atributo Nome é Obrigatório!")
 	private String nome;
 	
-	@NotBlank(message = " O NÚMERO DE TELEFONE é obrigatorio!!")
+	@NotBlank(message = " O número de telefone é obrigatorio!!")
 	@Pattern(regexp = "^\\+?[1-9]\\d{1,14}$")
 	private String telefone;
 
-	@NotNull(message = "O Atributo Usuário é Obrigatório!")
+	@NotNull(message = "O Atributo Usuário é obrigatório!")
 	@Email(message = "O Atributo Usuário deve ser um email válido!")
 	private String usuario;
 
-	@NotBlank(message = "O Atributo Senha é Obrigatório!")
+	@NotBlank(message = "O Atributo Senha é obrigatório!")
 	@Size(min = 8, message = "A Senha deve ter no mínimo 8 caracteres")
 	private String senha;
 
 	@Size(max = 5000, message = "O link da foto não pode ser maior do que 5000 caracteres")
 	private String foto;
-
-	/* Insira os Getters and Setters */
+	
+	@NotNull(message = "Altura não pode ser nula")
+	private Double altura;  
+	
+	@NotNull(message = "Peso não pode ser nulo")
+    private Double peso;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "usuario", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("usuario")
+	private List<Treinos> treino;
 
 	public Long getId() {
-		return this.id;
+		return id;
 	}
 
 	public void setId(Long id) {
@@ -48,15 +63,23 @@ public class Usuario {
 	}
 
 	public String getNome() {
-		return this.nome;
+		return nome;
 	}
 
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
 
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
 	public String getUsuario() {
-		return this.usuario;
+		return usuario;
 	}
 
 	public void setUsuario(String usuario) {
@@ -64,7 +87,7 @@ public class Usuario {
 	}
 
 	public String getSenha() {
-		return this.senha;
+		return senha;
 	}
 
 	public void setSenha(String senha) {
@@ -72,11 +95,28 @@ public class Usuario {
 	}
 
 	public String getFoto() {
-		return this.foto;
+		return foto;
 	}
 
 	public void setFoto(String foto) {
 		this.foto = foto;
 	}
 
+	public Double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(Double altura) {
+		this.altura = altura;
+	}
+
+	public Double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(Double peso) {
+		this.peso = peso;
+	}    
+
+	
 }
