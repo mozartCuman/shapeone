6d460d9d206cbaaac39d96490c5ae75af1d026d8
Felix Augusto <111309593+AugustoFO@users.noreply.github.com>
2024-12-10 11:07:43 -0300

    Inicializando o repositório

---
+++ src/main/java/com/shapeone/shapeone/ShapeOneApplication.java
@@ -0,0 +1,13 @@
+package com.shapeone.shapeone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShapeOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShapeOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shapeone/shapeone/ShapeOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shapeone.shapeone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShapeOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
